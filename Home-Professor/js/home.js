8f4c6cb793b5179cdccdc8f7184da13f86da0675
Lucas-Retanero <glucas.raro@gmail.com>
2024-10-13 21:17:31 -0300

    funcionalidade de limpar a pesquisa adicionada

--- Home-Professor/js/home.js
+++ Home-Professor/js/home.js
@@ -33,3 +33,43 @@ document.addEventListener('click', function (event) {
     let cardPerfil = document.querySelector('.perfil');
     fecharMenusSeClicarFora(event, iconePerfil, cardPerfil);
 });
+
+// Botão Limpa Input 1: 
+const pesquisaInput = document.getElementById('pesquisa');
+const clearButton = document.getElementById('limpa-input');
+
+// Adiciona evento ao input para monitorar a digitação
+pesquisaInput.addEventListener('input', function() {
+    if (this.value) {
+        clearButton.style.display = 'inline'; // Exibe o botão
+    } else {
+        clearButton.style.display = 'none'; // Esconde o botão
+    }
+});
+
+// Adiciona evento ao botão de limpar
+clearButton.addEventListener('click', function() {
+    pesquisaInput.value = ''; // Limpa o campo de input
+    clearButton.style.display = 'none'; // Esconde o botão
+    pesquisaInput.focus(); // Mantém o foco no campo de input
+});
+
+// Botão Limpa Input 2 
+const pesquisaInput2 = document.getElementById('pesquisa-2');
+const clearButton2 = document.getElementById('limpa-input2');
+
+// Adiciona evento ao input para monitorar a digitação
+pesquisaInput2.addEventListener('input', function() {
+    if (this.value) {
+        clearButton2.style.display = 'inline'; // Exibe o botão
+    } else {
+        clearButton2.style.display = 'none'; // Esconde o botão
+    }
+});
+
+// Adiciona evento ao botão de limpar
+clearButton2.addEventListener('click', function() {
+    pesquisaInput2.value = ''; // Limpa o campo de input
+    clearButton2.style.display = 'none'; // Esconde o botão
+    pesquisaInput2.focus(); // Mantém o foco no campo de input
+});
