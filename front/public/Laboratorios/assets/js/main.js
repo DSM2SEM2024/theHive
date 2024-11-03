ec48978da1fa4dbb1623c00e871ff5427d459664
Kevyn-Cavalcanti <contacontaconta0002@gmail.com>
2024-11-03 17:34:27 -0300

    labs

--- front/public/Laboratorios/assets/js/main.js
+++ front/public/Laboratorios/assets/js/main.js
@@ -1,17 +1,32 @@
-/*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
-  loop: true,
-  spaceBetween: 32,
-  grabCursor: true,
+fetch('http://localhost:3000/labs', {
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ nome, andar, equipamento, capacidade })
+})
+)
+.then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('container-de-cards');
+    container.innerHTML = ''; // Limpa o container
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    data.forEach(laboratorio => {
+      const card = document.createElement('article');
+      card.className = 'card-lab';
+      card.innerHTML =
+          <span id="barra-card"></span>
+          <h3 id="nome-lab"></h3>
+          <div class="lab-content">
+            <img id="img-lab" src="" alt="Imagem de laboratório">
+              <div class="descricao-lab">
+                <ul>
+                  <li id="capacidade"><strong>Capacidade:</strong><br> </li>
+                  <li id="equipamento"><strong>Equipamentos:</strong><br> </li>
+                </ul>
+              </div>
+          </div>
+          <button class="btn-calendario">Ver calendário</button>
+        ;
+      container.appendChild(card);
+    });
+  })
+  .catch(error => console.error('Erro ao buscar produtos:', error));
