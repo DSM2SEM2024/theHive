ab0a110b1e13ba0692c78eb6fe1a60a96e28da68
Guilherme Souza Alves <guilhermesza123@gmail.com>
2024-11-10 17:09:38 -0300

    passando para o Lucas fazer o metodo read

--- app/Model/Curso.php
+++ app/Model/Curso.php
@@ -15,12 +15,67 @@ class Curso {
         $this->conn = Database::getInstance();
     }
 
+    public function create(){
+        $query = "INSERT INTO $this->table (id_curso, nome, estado, data_cad) VALUES (:id_curso, :nome, :estado, :data_cad)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id_curso", $this->idCurso, PDO::PARAM_INT);
+        $stmt->bindParam(":nome", $this->nome, PDO::PARAM_STR);
+        $stmt->bindParam(":estado", $this->estado, PDO::PARAM_INT);
+        $stmt->bindParam(":data_cad", $this->dataCad, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+        
+    // Metodo para obter(gets) os cursos
+    public function read() {
+        // escreva aqui lucas retranqueiro
+        }
+
     public function atualizarEstadoCurso($idCurso, $novoEstado) {
-        $query = "UPDATE CURSO SET estado = :estado WHERE id_curso = :id_curso";
+        $query = "UPDATE $this->table SET estado = :estado WHERE id_curso = :id_curso";
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(":estado", $novoEstado, PDO::PARAM_INT);
         $stmt->bindParam(":id_curso", $idCurso, PDO::PARAM_INT);
         return $stmt->execute();
     }
 
+    public function delete($idCurso) {
+        $query = "DELETE FROM $this->table WHERE id_curso = :id_curso";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id_curso", $idCurso, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+
+    public function getCursoId() {
+        return $this->idCurso;
+    }
+    public function setCursoId($idCurso): self {
+        $this->idCurso = $idCurso;
+        return $this;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+    public function setNome($nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    public function setEstado($estado): self {
+        $this->estado = $estado;
+        return $this;
+    }
+
+    public function getDataCad() {
+        return $this->dataCad;
+    }
 }
+
+
+
+
+    
