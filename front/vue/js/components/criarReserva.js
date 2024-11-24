1706a94f55e8eabca66544971a0fe2dd77ebb94c
Hush <afonso.sprocatti@fatec.sp.gov.br>
2024-11-24 17:42:03 -0300

    Calendario e Criar Reserva 2.0

--- front/vue/js/components/criarReserva.js
+++ front/vue/js/components/criarReserva.js
@@ -1,29 +1,29 @@
 export const criarReserva = {
     template: `
-      <form @submit.prevent="criarEvento">
-        <select v-model="evento.titulo" required>
-          <option disabled value="">Selecione uma sala</option>
-          <option v-for="(cor, titulo) in salas" :key="titulo" :value="titulo">{{ titulo }}</option>
+      <form @submit.prevent="criarReserva">
+        <select v-model="reserva.titulo" required>
+          <option disabled value="">Selecione um laboratório</option>
+          <option v-for="laboratorio in labs" :key="laboratorio.nome" :value="laboratorio.nome">{{ laboratorio.nome }}</option>
         </select>
-        <input v-model="evento.descricao" placeholder="Descrição" />
+        <input v-model="reserva.descricao" placeholder="Descrição" />
         <div>
-        <input v-model="evento.datainicial" type="date" required />
-        <input v-model="evento.horarioinicial" type="time" required />
-        <input v-model="evento.datafinal" type="date" />
-        <input v-model="evento.horariofinal" type="time" required />
+        <input v-model="reserva.datainicial" type="date" required />
+        <input v-model="reserva.horarioinicial" type="time" required />
+        <input v-model="reserva.datafinal" type="date" />
+        <input v-model="reserva.horariofinal" type="time" required />
         </div>
-        <select v-model="evento.recorrencia">
+        <select v-model="reserva.recorrencia">
           <option value="nenhuma">Recorrência</option>
           <option value="diaria">Diária</option>
           <option value="semanal">Semanal</option>
           <option value="mensal">Mensal</option>
           <option value="semestral">Semestral</option>
         </select>
-        <select v-model="evento.nome" required>
+        <select v-model="reserva.nome" required>
           <option disabled value="">Nome do usuário</option>
           <option v-for="user in usuarios" :key="user.nome" :value="user.nome">{{ user.nome }}</option>
         </select>
-        <button type="submit">Criar Evento</button>        
+        <button type="submit">Criar reserva</button>        
         <p v-if="mensagem">{{ mensagem }}</p>   
         <button @click="this.$router.push('/Calendario')" class="calendar-btn">Ver Calendário</button>     
       </form>
@@ -32,31 +32,25 @@ export const criarReserva = {
     props: ['urlbase'],
     data() {
         return { 
-            evento: {
-                titulo: '', 
-                descricao: '', 
-                datainicial: '',
-                horarioinicial: '', 
-                datafinal: '',
-                horariofinal: '',  
-                recorrencia: 'nenhuma', 
-                nome: '',
-                cor: ''
+            reserva: {
+                "usuarioId": null,
+                "laboratorioId": null,
+                "disciplinaId": null,
+                "datainicial": '',
+                "datafinal": '',
+                "horarioinicial": '',
+                "horariofinal": '',
+                "recorrencia": '',
+                "descricao": '',
+                "status":''
             },
             mensagem: '',
-            salas: {
-                "Sala A": "#FF5733",
-                "Sala B": "#33FF57",
-                "Sala C": "#3357FF",
-                "Sala D": "#FF33A1",
-                "Sala E": "#A133FF"
-            },
+            labs: [],
             usuarios:[]
         };
     },
     methods: {
-        async criarEvento() {
-            this.evento.cor = this.salas[this.evento.titulo];
+        async criarReserva() {
             const token = localStorage.getItem('token');
             const response = await fetch(`http://localhost:3000/reserve`, {
                 method: 'POST',
@@ -64,14 +58,15 @@ export const criarReserva = {
                     'Content-Type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 },
-                body: JSON.stringify(this.evento)
+                body: JSON.stringify(this.reserva)
             });
             const result = await response.json();
             if (result.status) {
-                this.mensagem = 'Evento(s) criado(s) com sucesso.';
-                this.$emit('eventoCriado');
+                this.mensagem = 'reserva(s) criado(s) com sucesso.';
+                console.log(this.reserva);
+                this.$emit('reservaCriado');
             } else {
-                this.mensagem = 'Evento não criado.';
+                this.mensagem = 'reserva não criado.';
             }
         },
         async buscaUsuarios() {
@@ -85,8 +80,21 @@ export const criarReserva = {
             });
             this.usuarios = await response.json();
         },
+        async buscaLabs() {
+            const token = localStorage.getItem('token');
+            const response = await fetch(`http://localhost:3000/labs`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            this.labs = await response.json();
+        },
     },
     async mounted() {
-       await this.buscaUsuarios()
+       await this.buscaUsuarios(),
+       await this.buscaLabs()
     },
+    
 };
