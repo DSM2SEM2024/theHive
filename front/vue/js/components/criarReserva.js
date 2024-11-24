db8586ade965d073b89f26aa1e273d8a4c07105a
KaryanthePierrot <mesquita.westesoficial@gmail.com>
2024-11-24 17:37:39 -0300

    css

--- front/vue/js/components/criarReserva.js
+++ front/vue/js/components/criarReserva.js
@@ -1,33 +1,57 @@
 export const criarReserva = {
     template: `
-      <form @submit.prevent="criarEvento">
-        <select v-model="evento.titulo" required>
-          <option disabled value="">Selecione uma sala</option>
+<div class="geral">
+      <form @submit.prevent="criarEvento" id="formularioReserva">
+        <select v-model="evento.titulo" id ="selecionar-sala" required>
+        
+        // selecionar sala
+        <option disabled value="">Selecione uma sala</option>
+
+          // exibir salas
           <option v-for="(cor, titulo) in salas" :key="titulo" :value="titulo">{{ titulo }}</option>
         </select>
-        <input v-model="evento.descricao" placeholder="Descrição" />
-        <div>
-        <input v-model="evento.datainicial" type="date" required />
-        <input v-model="evento.horarioinicial" type="time" required />
-        <input v-model="evento.datafinal" type="date" />
-        <input v-model="evento.horariofinal" type="time" required />
+
+
+         <!--descrição do evento-->
+            <textarea v-model="evento.descricao" maxlength="500" rows="5" cols="40" placeholder="Descrição do evento" id="descricao"/>
+
+        <div id="data-horario">
+            <input v-model="evento.datainicial" type="date"  id="datainicial" class="inputData-Hora" required />
+            <input v-model="evento.horarioinicial" type="time" id="horainicial" class="inputData-Hora" required />
+<br>
+            <input v-model="evento.datafinal" type="date" id="datafinal" class="inputData-Hora" required />
+            <input v-model="evento.horariofinal" type="time" id ="horafinal" class="inputData-Hora" required />
         </div>
-        <select v-model="evento.recorrencia">
+
+        <!-- recorrencia do evento -->
+        <select v-model="evento.recorrencia" id="recorrencia">
           <option value="nenhuma">Recorrência</option>
           <option value="diaria">Diária</option>
           <option value="semanal">Semanal</option>
           <option value="mensal">Mensal</option>
           <option value="semestral">Semestral</option>
         </select>
+
+
         <select v-model="evento.nome" required>
           <option disabled value="">Nome do usuário</option>
           <option v-for="user in usuarios" :key="user.nome" :value="user.nome">{{ user.nome }}</option>
         </select>
-        <button type="submit">Criar Evento</button>        
+
+        <br>
+
+        <button type="submit" id='btnCria'>Criar Reserva</button>  
+        
+        
         <p v-if="mensagem">{{ mensagem }}</p>   
+        
+        
         <button @click="this.$router.push('/Calendario')" class="calendar-btn">Ver Calendário</button>     
+        
+        <br>
+
       </form>
-      
+      </div>
     `,
     props: ['urlbase'],
     data() {
