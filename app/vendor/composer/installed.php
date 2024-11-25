8b7ac32ddc781c4012d7cc3f845b3272d2651a05
ViniMazWolf <viniciusmazs@gmail.com>
2024-11-25 19:33:30 -0300

    vendor está no git ahora

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'conta/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c96f5b91e00a34574fa851e44d408333d725401c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'conta/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c96f5b91e00a34574fa851e44d408333d725401c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.2',
+            'version' => '6.10.2.0',
+            'reference' => '30c19ed0f3264cb660ea496895cfb6ef7ee3653b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
