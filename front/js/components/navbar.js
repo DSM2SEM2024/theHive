ac4b30f8e1e09a03e388a8374f43c02b7255ca33
Kevyn-Cavalcanti <contacontaconta0002@gmail.com>
2024-11-27 11:59:53 -0300

    Para a apresentação

--- front/js/components/navbar.js
+++ front/js/components/navbar.js
@@ -277,16 +277,15 @@ export const Navbar = {
           <header>
             <h1 v-if="!logado">Fatec - SALA</h1>
               <a v-if="logado" @click="this.$router.push('/home');"><h1 id="tituloFatec">Fatec - SALA</h1></a>
-              <i title="Início" v-if="logado" id="icon-home" class="fi fi-ss-home"  @click="this.$router.push('/home');"></i>
               <div id="icons" v-if="logado && this.professor">
+                    <i class="fi fi-ss-home" ref="iconHome" @click="this.$router.push('/home');"></i>
                     <i class="fi fi-ss-bell" ref="iconNotificacoes" @click="alteraNotificacoes"></i>
                     <i class="fi fi-ss-circle-user" ref="iconPerfil" @click="alteraPerfil"></i>
               </div>
               <div id="icons" v-if="logado && !this.professor">
+                    <i class="fi fi-ss-home" ref="iconHome" @click="this.$router.push('/home');"></i>
                     <i id="btn-notificacoes" class="fi fi-ss-bell" ref="iconNotificacoes" @click="alteraNotificacoes"></i>
                     <i id="btn-usuario" ref="iconUsuario" @click="this.$router.push('/usuarios');" class="fi fi-ss-admin-alt"></i>
-                    <i id="btn-equipamentos" ref="iconEquipamentos" @click="this.$router.push('/equipamentos');" class="fi fi-ss-admin-alt"></i>
-                    <i id="btn-lixeira" ref="iconLixeira" @click="this.$router.push('/lixeira');" class="fi fi-ss-trash-can-clock"></i>
                     <i id="btn-perfil" class="fi fi-ss-circle-user" ref="iconPerfil" @click="alteraPerfil"></i>
               </div>
           </header>
